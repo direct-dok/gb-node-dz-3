15eadfae11179d296f2b5d1184df024ac9b0545c
Viktor <you@example.com>
2021-11-22 21:48:58 +0500

    pull request

--- index.js
+++ index.js
@@ -1,29 +1,65 @@
 const fs = require('fs')
-const fsPromises = require('fs/promises')
-const ACCESS_LOG = './string.log'
+const { Transform } = require('stream')
+const ACCESS_LOG = './access.log'
+const WRITE_LOG = './write.log'
+
+// const readStream = fs.createReadStream(
+//     ACCESS_LOG, 
+//     {
+//         flags: 'r',
+//         encoding: 'utf-8',
+//         autoClose: true,
+//         // start: 0,
+//         // end: 6,
+//         highWaterMark: 110,
+//     }
+// )
+
+
+
+// readStream.on('data', chunk => {
+//     console.log('*** - ', chunk);
+// })
+
+// const writeStream = fs.createWriteStream(
+//     ACCESS_LOG, 
+//     {
+//         encoding: 'utf-8',
+//         flags: 'a'
+//     }
+// )
 
 const readStream = fs.createReadStream(
     ACCESS_LOG, 
     {
         flags: 'r',
         encoding: 'utf-8',
-        // autoClose: true,
-        // start,
-        // end,
-        highWaterMark: 64,
+        autoClose: true,
+        // start: 0,
+        // end: 6,
+        highWaterMark: 10,
     }
 )
 
-readStream.on('data', chunk => {
-    console.log('chunk - ', chunk);
-})
 
 const writeStream = fs.createWriteStream(
-    ACCESS_LOG, 
+    WRITE_LOG, 
     {
         encoding: 'utf-8',
         flags: 'a'
     }
 )
 
+const tStream = new Transform({
+    transform(chunk, encoding, callback) {
+        const transformed = "---- *** " + chunk.toString()
+        this.push(transformed)
+        if(transformed.includes('127.0.0.1')) {
+            writeStream.write(transformed);
+        }
+        
+        callback()
+    }
+})
 
+readStream.pipe(tStream).pipe(process.stdout);
